64112c408e7ae23a27251f6603c34863994edca4
lz10hnr <923622583@.com>
2017-10-18 10:48:09 +0800

    添加了日志输出

--- app/src/main/java/com/jiyun/a03aproject/MainActivity.java
+++ app/src/main/java/com/jiyun/a03aproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.jiyun.a03aproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.d("MainActivity", "03a");
+        Toast.makeText(this, "03b", Toast.LENGTH_SHORT).show();
+
+
+
+
+
     }
 }
